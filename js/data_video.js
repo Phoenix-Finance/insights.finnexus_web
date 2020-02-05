df75af86d13ee558ebb3f2833ce9a1281dffd754
Astro9113 <astro913@126.com>
2020-02-05 19:48:00 +0800

    update

---
+++ js/data_video.js
@@ -0,0 +1,135 @@
+var data = {
+
+
+    // 个人简介
+    personal: {
+        list: [
+            [{
+                    title: 'Alex Preukschat',
+                    partner: 'Alianza Blockchain',
+                    img: '../presenter/AlexPreukschat.png',
+                    arrington: '"Lessons in Personal Development from Bitcoin and Blockchain / Identity is the New Money"',
+                    video: ['qx2EXUX3NUE','K6zFlmSdkDA'],
+                },
+                {
+                    title: 'Rossco Paddison',
+                    partner: 'Mosendo',
+                    img: '../presenter/RosscoPaddison.png',
+                    arrington: '"What will it take for Defi to replace legacy financial systems?"',
+                    video: ['sr1_3bdOy9I'],
+                },
+                {
+                    title: 'Galen Danziger',
+                    partner: 'MouseBelt',
+                    img: '../presenter/Galen.png',
+                    arrington: '"Why the crypto VC model is broken"',
+                    video: ['w7HaxHh3-hs'],
+                }
+            ],
+            [{
+                    title: 'Peter van Mourik',
+                    partner: 'ChainLayer',
+                    img: '../presenter/PetervanMourik.png',
+                    arrington: '"How validators are changing blockchain ecosystems"',
+                    video: ['2vi8M3evwKk'],
+                },
+                {
+                    title: 'Siddharth Verma',
+                    partner: 'Nuo Network',
+                    img: '../presenter/Siddharthverma.png',
+                    arrington: '"Bringing de-fi to masses and future of neo-banking"',
+                    video: ['cDaaSif-pmc'],
+                },
+                {
+                    title: 'Bill Laboon',
+                    partner: 'Web3 Foundation',
+                    img: '../presenter/BillLaboon.png',
+                    arrington: '"An introduction to Polkadot"',
+                    video: ['U_kgwVIW4pA'],
+                }
+            ],
+            [{		
+                    title: 'Bruce Li',
+                    partner: 'NKN.org',
+                    img: '../presenter/bruceli.png',
+                    arrington: '"Communication as a service powered by blockchain"',
+                    video: ['bRklwbKNxiQ'],
+                },
+                {
+                    title: 'Justin Cai',
+                    partner: 'Longhash',
+                    img: '../presenter/JustinCai.png',
+                    arrington: '"Leveraging on-chain data analysis to generate insight on the blockchain ecosystem"',
+                    video: ['WnmhMG7FWmw'],
+                },
+                {		
+                    title: 'Boyd Cohen',
+                    partner: 'Iomob',
+                    img: '../presenter/BoydCohen.png',
+                    arrington: '"From native token to enterprise sales to the decentralisation of the global mobility ecosystem"',
+                    video: ['ZrfLuPhmVzo'],
+                }
+            ],
+            [{
+                    title: 'Dan Liebau',
+                    partner: 'Lightbulb Capital',
+                    img: '../presenter/DanLiebau.png',
+                    arrington: '"Cryptocurrencies and ICOs: Are they scams? An empirical analysis"',
+                    video: ['tcVXbEL7sF8'],
+                },
+                {
+                    title: 'Soravis Srinawakoon',
+                    partner: 'Band Protocol',
+                    img: '../presenter/SoravisSrinawakoon.png',
+                    arrington: '"An in depth conversation about blockchain oracles with Soravis"',
+                    video: ['4KQkPM9ekR0'],
+                },
+                {
+                    
+                    title: 'Aaron Choi',
+                    partner: 'Kava',
+                    img: '../presenter/Aaron.png',
+                    arrington: '"Defi - How did we get here?"',
+                    video: ['nKmc_QqJy5E'],
+                },
+
+            ],
+
+            [{
+                title: 'David Johnston',
+                partner: 'Pegnet',
+                img: '../presenter/David.png',
+                arrington: '"Fully-decentralized Mineable Stablecoins are the Future"',
+                video: ['4utskKyqTbs'],
+            },
+            {
+                title: 'Peko Wan',
+                partner: 'Pundi X',
+                img: '../presenter/Peko.png',
+                arrington: '"Blockchain Payment 2020"',
+                video: ['KsIjPkBnFR0'],
+            },
+            {
+                
+                title: 'Alexander Chepurnoy',
+                partner: 'Ergo',
+                img: '../presenter/Alexander.png',
+                arrington: '"A New Quest for Decentralization: A Technical Perspective"',
+                video: ['spex9o_Ceck'],
+            },
+            
+        ],
+        [{
+            title: 'Joaquin Moreno',
+            partner: 'Alianza Blockchain',
+            img: '../presenter/Joaquin.png',
+            arrington: '"Latin America Blockchain Industry Report – hosted by Wanchain’s Oliver Birch"',
+            video: ['rlX4cW7bPuw'],
+        },
+        
+    ],
+
+        ]
+    }
+
+}
--- js/video.js
+++ js/video.js
@@ -20,23 +20,34 @@ $(function () {
     // console.log('item', item);
 
     var personalHtml = '<div class="personal-video-centext"><dl>'
-        + '<dt><img src="./img/' + item.img + '" alt="' + item.vimeo + '" style="width: 88px;border-radius: 50px;filter:grayscale(100%);-webkit-filter:grayscale(100%)" ' + '></dt>'
-        + '<dd>' + '<h3>' + item.title + '</h3>' + '<b><img src="" alt=""></b>' + '<p>' + item.partner + '</p>'
+        + '<dt><img src="./img/' + item.img + '" style="width: 88px;border-radius: 50px;filter:grayscale(100%);-webkit-filter:grayscale(100%)" ' + '></dt>'
+        + '<dd>' + '<h3>' + item.title + '</h3>' + '<b><img src=""></b>' + '<p>' + item.partner + '</p>'
         + '<span>' + item.arrington + '</span>' + '</dd></dl></div>'
 
 
 
         $('.personal-profile-title').html(personalHtml);
+    
+    var video_length = item.video.length;
+    if (video_length >1) {
+        var videoHtml = [];
 
-//      var videoFrame = `<iframe src="` + item.video + `" 
-//         height="500" frameborder="0" allow="autoplay; fullscreen" allowfullscreen>
-//    </iframe>`
+        item.video.forEach(function (v) {
+            videoHtml.push('<iframe class = "video_frame" '  
+            + 'src="' + 'https://www.youtube.com/embed/' + v +
+            '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+        });
 
-   var videoFrame = '<iframe width="80%" height="500"  style ="display: block; margin: 0 auto;" '  
-        + 'src="' + item.video +
+        $('.modal-content').append(videoHtml.join(''));
+    } else {
+        var videoFrame = '<iframe width="60%" height="500"  style ="display: block; margin: 0 auto;" '  
+        + 'src="' + 'https://www.youtube.com/embed/' + item.video[0] +
         '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
 
        $('.modal-content').html(videoFrame);
+    }
+
+    
 
 })
 
